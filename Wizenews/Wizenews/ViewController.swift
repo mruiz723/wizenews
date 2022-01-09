9f7c34a2c94d523669d75ffbe7e3049767a2693a
Marlon Ruiz <marlon.ruiz@wizeline.com>
2022-01-08 20:28:10 -0500

    Created the initial project.

---
+++ Wizenews/Wizenews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wizenews
+//
+//  Created by Marlon David Ruiz Arroyave on 8/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
